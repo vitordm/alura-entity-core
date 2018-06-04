1882cf89e34f20a7e1226627e6ae651ab4dcfce5
Vitor Gaudencio Isse de Oliveira <oliveira.vitor3@gmail.com>
2018-06-03 22:23:29 -0300

    Aula 1 - Configurando nomes de tabelas e colunas

---
+++ src/Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContext.cs
@@ -0,0 +1,15 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AluraFilmesContext : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer($"Server=192.168.0.25;Database=AluraFilmes;User Id=SA;Password={DatabasePassword.Password};");
+        }
+    }
+}
--- src/Alura.Filmes/Alura.Filmes.App/Program.cs
+++ src/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using System;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            using (var context = new AluraFilmesContext())
+            {
+                context.LogSQLToConsole();
+                foreach (var ator in context.Atores)
+                {
+                    Console.WriteLine($"{ator.Id} - {ator.PrimeiroNome}");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
