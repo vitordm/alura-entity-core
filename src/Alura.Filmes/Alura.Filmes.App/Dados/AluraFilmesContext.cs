e6cfa533e43d9076b7e960fd76899c06ad4803ec
Vitor Oliveira <oliveira.vitor3@gmail.com>
2018-06-08 14:18:55 -0300

    Aula 2 -  Refinando o mapeamento da tabela de atores

--- src/Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContext.cs
+++ src/Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContext.cs
@@ -9,7 +9,27 @@ namespace Alura.Filmes.App.Dados
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer($"Server=192.168.0.25;Database=AluraFilmes;User Id=SA;Password={DatabasePassword.Password};");
+            optionsBuilder.UseSqlServer($"Server=172.16.8.104;Database=AluraFilmes;User Id=SA;Password={DatabasePassword.Password};");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Ator>().ToTable("actor");
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.Id)
+                .HasColumnName("actor_id");
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.PrimeiroNome)
+                .HasColumnName("first_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.UltimoNome)
+                .HasColumnName("last_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
         }
     }
 }
--- src/Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
+++ src/Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -8,16 +8,11 @@ using System.Threading.Tasks;
 
 namespace Alura.Filmes.App.Negocio
 {
-    [Table("actor")]
     public class Ator
     {
-        [Column("actor_id")]
+
         public int Id { get; set; }
-        [Required]
-        [Column("first_name", TypeName ="varchar(45)")]
         public string PrimeiroNome { get; set; }
-        [Required]
-        [Column("last_name", TypeName = "varchar(45)")]
         public string UltimoNome { get; set; }
     }
 }
