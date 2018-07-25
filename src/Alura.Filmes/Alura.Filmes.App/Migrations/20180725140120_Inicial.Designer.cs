e04b7ec1e8d7f4e1837b25bbb0b39443c0119ec6
Vitor Oliveira <oliveira.vitor3@gmail.com>
2018-07-25 11:34:23 -0300

    refazendo a migration

--- src/Alura.Filmes/Alura.Filmes.App/Migrations/20180725140120_Inicial.Designer.cs
+++ src/Alura.Filmes/Alura.Filmes.App/Migrations/20180725140120_Inicial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Alura.Filmes.App.Migrations
 {
     [DbContext(typeof(AluraFilmesContext))]
-    partial class AluraFilmesContextModelSnapshot : ModelSnapshot
+    [Migration("20180725140120_Inicial")]
+    partial class Inicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
