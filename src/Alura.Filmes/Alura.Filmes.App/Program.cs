fb0bb6fb8026db7256e66597717378bf9f3a5fb0
Vitor Oliveira <oliveira.vitor3@gmail.com>
2018-08-01 11:25:48 -0300

    Aula 5- Usando a View

--- src/Alura.Filmes/Alura.Filmes.App/Program.cs
+++ src/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -18,12 +18,7 @@ namespace Alura.Filmes.App
                 context.LogSQLToConsole();
                 var top5Atores = context.Atores.FromSql(@" select a.*
         from actor a
-            inner join
-        (select top 5 a.actor_id, count(*) as total
-        from actor a
-            inner join film_actor fa on fa.actor_id = a.actor_id
-        group by a.actor_id
-        order by total desc) filmes on filmes.actor_id = a.actor_id")
+            inner join top5_most_starred_actors filmes on filmes.actor_id = a.actor_id")
                 .Include(a => a.Filmografia);
 
                 foreach (var ator in top5Atores)
